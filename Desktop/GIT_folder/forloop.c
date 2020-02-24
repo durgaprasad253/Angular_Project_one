612da4eb1de178110326177131d62a5daf1c036a
AmitKarnam <AmitKarnam@01.com>
2020-02-25 00:13:44 +0530

    Useful ides list

---
+++ Desktop/GIT_folder/forloop.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+	n=2;
+	for(int i=0;i<n;i++)
+		pritnf("I am Awesome");
+}
---
+++ Desktop/Git_merge_concept/hello.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+
+{
+	int a,b;
+	int sum = a + b;
+	printf("%d",sum);
+
+
+}
